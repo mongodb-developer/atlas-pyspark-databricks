0ff92d3d0522a6af1008d74974e4ee504a34edcf
mongoanaiya <86017775+mongoanaiya@users.noreply.github.com>
2023-02-06 17:36:05 -0500

    formatting and cleaning up comments

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 from dotenv import load_dotenv
 import os
 
-# use environment variable for uri 
+# Use environment variable for uri.
 load_dotenv()
 connection_string: str = os.environ.get("CONNECTION_STRING")
 
@@ -15,12 +15,12 @@ my_spark = SparkSession \
     .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector:10.0.3") \
     .getOrCreate()
 
-
-# Create a data frame so you can write to your bookshelf in your Atlas cluster
+# Create a data frame so you can write to your bookshelf in your Atlas cluster.
 add_books = my_spark.createDataFrame([("War and Peace", "Leo Tolstoy", 1867)], ["title", "author", "year"])
 add_books.write.format("com.mongodb.spark.sql.DefaultSource").option('uri', connection_string).option('database', 'bookshelf').option('collection', 'books').mode("append").save()
 
-# Create a data frame so you can read in your books from your bookshelf
+# Create a data frame so you can read in your books from your bookshelf.
 return_books = my_spark.read.format("com.mongodb.spark.sql.DefaultSource").option('uri', connection_string).option('database', 'bookshelf').option('collection', 'books').load()
-# Show the books in your PySpark shell
+
+# Show the books in your PySpark shell.
 return_books.show()
