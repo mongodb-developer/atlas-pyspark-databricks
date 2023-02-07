3be721c9c4e6ed51c9b9e95c7875ccaa2222c215
mongoanaiya <86017775+mongoanaiya@users.noreply.github.com>
2023-02-07 11:43:55 -0500

    formatting

--- demo.py
+++ demo.py
@@ -16,11 +16,17 @@ my_spark = SparkSession \
     .getOrCreate()
 
 # Create a data frame so you can write to your bookshelf in your Atlas cluster.
-add_books = my_spark.createDataFrame([("War and Peace", "Leo Tolstoy", 1867)], ["title", "author", "year"])
-add_books.write.format("com.mongodb.spark.sql.DefaultSource").option('uri', connection_string).option('database', 'bookshelf').option('collection', 'books').mode("append").save()
+add_books = my_spark \
+    .createDataFrame([("War and Peace", "Leo Tolstoy", 1867)], ["title", "author", "year"])
+
+add_books.write.format("com.mongodb.spark.sql.DefaultSource").option('uri', connection_string).option('database', 'bookshelf').option('collection', 'books').mode("append").save() 
 
 # Create a data frame so you can read in your books from your bookshelf.
-return_books = my_spark.read.format("com.mongodb.spark.sql.DefaultSource").option('uri', connection_string).option('database', 'bookshelf').option('collection', 'books').load()
+return_books = my_spark.read.format("com.mongodb.spark.sql.DefaultSource") \
+    .option('uri', connection_string) \
+    .option('database', 'bookshelf') \
+    .option('collection', 'books') \
+    .load()
 
 # Show the books in your PySpark shell.
 return_books.show()
